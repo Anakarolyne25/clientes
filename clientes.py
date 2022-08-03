d251ba47e2bcfa31bae64e31c02b77279f2e0a49
Fabrício Jonatan Martins Nascimento <110259064+FabricioJonatan@users.noreply.github.com>
2022-08-03 16:14:18 -0300

    Deixa as coisas mais bonitas.
    Alterei alguns prints e inputs para eles estarem mais atraentes aos usuários.

--- clientes.py
+++ clientes.py
@@ -1,41 +1,34 @@
+NOME_ARQUIVO = "clientes.txt"
 clientes = []
-
 def cadastrar():
-    while True:
-        print('cadastrando \n')
-        nome = input('Digite o nome do cliente : ').capitalize().strip()
-        telefone = input('Digite o telefone do cliente : ')
-        cliente = [nome, telefone]
-        salvarArquivo(cliente)
-        if cadastrarNovamente() == False:
-            break
+    print('cadastrando')
+    nome = input('Digite o nome : ')
+    telefone = input('Digite o telefone : ')
+    cliente = [nome, telefone]
+    salvarArquivo(cliente)
 
-def salvarArquivo(cliente):
-    with open('contatos.txt', 'a') as arquivo:
-        arquivo.write(f'{cliente[0]} {cliente[1]} \n')
+def salvar(cliente):
+    clientes.append(cliente)
 
-def cadastrarNovamente():
-    while True:
-        continuar = input('Deseja cadastrar outro cliente? [S/N] ').lower().strip()
-
-        if continuar in 'sn':
-            if continuar == 's':
-                return True
-            else:
-                return False
-        else:
-            print('Resposta Inválida!')
-
-def buscarLista():
+def buscarClientes():
     return clientes
 
 def listar():
-    clientes = buscarLista()
+    clientes = buscarClientesArquivo()
     print('listando')
     print(f'{"nome":^10}\t{"telefone":^10}')
     for cliente in clientes:
         print(f'{cliente[0]:10}\t{cliente[1]:10}')
 
+def salvarArquivo(cliente):
+    arquivo = open(NOME_ARQUIVO, "a")
+    arquivo.write(f'{cliente[0]} {cliente[1]}\n')
+    arquivo.close()
+
+def buscarClientesArquivo():
+    arquivo = open(NOME_ARQUIVO, "r") 
+    for linha in arquivo:
+        print(linha)
 
 while True:
     print('''
@@ -48,8 +41,6 @@ while True:
         cadastrar()
     elif opcao == 2:
         listar()
-    elif opcao == 3: 
+    else: 
         print('exit')
         break 
-    else:
-        print('Opção inválida, por favor digite novamente.')
