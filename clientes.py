c306b518668b135c7b99aff416f02a601ec7bda1
Fabrício Jonatan Martins Nascimento <110259064+FabricioJonatan@users.noreply.github.com>
2022-08-02 21:37:48 -0300

    Interface e Funcionalidade mais Atraente
    Alterei algumas coisinhas para tornar o programa mais bonito de alguma forma para ser mais fácil do usuário utilizar, também alterei um pouco o sistema pra salvar os dados em arquivos e adicionei algumas funcionalidades como a de pedir outro numero caso o digitado não esteja nas opções (1. cadastrar, 2. listar e 3. sair) e a opção do usuário adicionar vários cadastros sem voltar ao menu inicial.

--- clientes.py
+++ clientes.py
@@ -1,26 +1,55 @@
 clientes = []
+
 def cadastrar():
-    print('cadastrando')
-    nome = input('Digite o nome')
-    telefone = input('Digite o telefone')
-    cliente = [nome, telefone]
-    clientes.append(cliente)
+    while True:
+        print('cadastrando \n')
+        nome = input('Digite o nome do cliente : ').capitalize().strip()
+        telefone = input('Digite o telefone do cliente : ')
+        cliente = [nome, telefone]
+        salvarArquivo(cliente)
+        if cadastrarNovamente() == False:
+            break
+
+def salvarArquivo(cliente):
+    with open('contatos.txt', 'a') as arquivo:
+        arquivo.write(f'{cliente[0]} {cliente[1]} \n')
+
+def cadastrarNovamente():
+    while True:
+        continuar = input('Deseja cadastrar outro cliente? [S/N] ').lower().strip()
+
+        if continuar in 'sn':
+            if continuar == 's':
+                return True
+            else:
+                return False
+        else:
+            print('Resposta Inválida!')
+
+def buscarLista():
+    return clientes
 
 def listar():
+    clientes = buscarLista()
     print('listando')
     print(f'{"nome":^10}\t{"telefone":^10}')
     for cliente in clientes:
         print(f'{cliente[0]:10}\t{cliente[1]:10}')
 
+
 while True:
-    print('1.cadastrar')
-    print('2.listar')
-    print('3.sair')
-    opcao = int(input('Qual das opções desejada?'))
+    print('''
+    [ 1 ]. Cadastrar
+    [ 2 ]. Listar
+    [ 3 ]. Sair
+    ''')
+    opcao = int(input('Qual das opções desejada? '))
     if opcao == 1:
         cadastrar()
     elif opcao == 2:
         listar()
-    else: 
+    elif opcao == 3: 
         print('exit')
         break 
+    else:
+        print('Opção inválida, por favor digite novamente.')
