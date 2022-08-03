d500ef482fe0873b54d3a2c5fceccb184af36436
leonardo brito <110542070+97394474@users.noreply.github.com>
2022-08-03 11:08:51 -0300

    comecar a ler arquivo

--- clientes.py
+++ clientes.py
@@ -1,3 +1,4 @@
+NOME_ARQUIVO = "clientes.txt"
 clientes = []
 def cadastrar():
     print('cadastrando')
@@ -9,22 +10,25 @@ def cadastrar():
 def salvar(cliente):
     clientes.append(cliente)
 
-def buscarLista():
+def buscarClientes():
     return clientes
 
 def listar():
-    clientes = buscarLista()
+    clientes = buscarClientesArquivo()
     print('listando')
     print(f'{"nome":^10}\t{"telefone":^10}')
     for cliente in clientes:
         print(f'{cliente[0]:10}\t{cliente[1]:10}')
 
 def salvarArquivo(cliente):
-    arquivo = open("contatos.txt", "a")
-    arquivo.write(f'{cliente[0]} {cliente[1]}')
+    arquivo = open(NOME_ARQUIVO, "a")
+    arquivo.write(f'{cliente[0]} {cliente[1]}\n')
     arquivo.close()
 
-
+def buscarClientesArquivo():
+    arquivo = open(NOME_ARQUIVO, "r") 
+    for linha in arquivo:
+        print(linha)
 
 while True:
     print('1.cadastrar')
